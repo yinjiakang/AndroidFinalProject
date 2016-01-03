21eca021c36a2fb12eb2b79fca1d61c53cb3a74f
J.K yin <yinjiakang@qq.com>
2016-01-03 12:02:41 +0800

    Add operation in login, signup

--- SingForYou/src/com/example/singforyou/LoginActivity.java
+++ SingForYou/src/com/example/singforyou/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.singforyou;
 
+import android.R.string;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 public class LoginActivity extends Activity {
 	private Button Login_TurnToRegister, Login_LoginBtn;
 	private EditText Login_accounts, Login_password;
+	protected static Person person;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +21,29 @@ public class LoginActivity extends Activity {
 		Login_LoginBtn = (Button) findViewById(R.id.login);
 		Login_accounts = (EditText) findViewById(R.id.accounts);
 		Login_password = (EditText) findViewById(R.id.password);
+		
+		Login_LoginBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String account = Login_accounts.getText().toString();
+				String password = Login_password.getText().toString();
+				/*
+				 * need db
+				 * post    account
+				 * if exists , return Person
+				 * else return empty string
+				 */
+				
+				/* 
+				 * if (person.getPassword().equals(password)) {
+				 * 		Intent intent = new Intent(LoginActivity.this, TiebaActivity.class);
+				 * 		startActivity(intent);
+				 * } else {
+				 * 		// error
+				 * }
+				 */
+			}
+		});
 		
 		Login_TurnToRegister.setOnClickListener(new OnClickListener() {
 			@Override
--- SingForYou/src/com/example/singforyou/SignupActivity.java
+++ SingForYou/src/com/example/singforyou/SignupActivity.java
@@ -9,21 +9,30 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class SignupActivity extends Activity {
-	private EditText Signup_name, Signup_password;
+	private EditText Signup_account, Signup_password;
 	private Button Signup_register_btn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
-		Signup_name = (EditText) findViewById(R.id.name);
+		Signup_account = (EditText) findViewById(R.id.name);
 		Signup_password = (EditText) findViewById(R.id.password);;
 		Signup_register_btn = (Button) findViewById(R.id.register_btn);
 		
 		Signup_register_btn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// implement needed
+				String account = Signup_account.getText().toString();
+				String password = Signup_password.getText().toString();
+				/*
+				 * need db
+				 * post    account
+				 * if exists , return Person
+				 * else return empty string
+				 */
 				
+				Intent intent = new Intent(SignupActivity.this, TiebaActivity.class);
+				startActivity(intent);
 			}
 		});
 	}
