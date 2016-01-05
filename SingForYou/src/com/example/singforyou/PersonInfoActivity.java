370e8425b5230d8bcca8549a820b9daddf88c40c
yf416656261 <416656261@qq.com>
2016-01-05 22:29:09 +0800

    个人信息区

--- SingForYou/src/com/example/singforyou/PersonInfoActivity.java
+++ SingForYou/src/com/example/singforyou/PersonInfoActivity.java
@@ -1,16 +1,32 @@
 package com.example.singforyou;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
 
 public class PersonInfoActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.person_info_activity);
-	}
 	
-	
+	private TextView name, sex, tel, mail, add;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.person_info_activity);
+        name = (TextView)findViewById(R.id.person_name);
+        sex = (TextView)findViewById(R.id.person_sex);
+        tel = (TextView)findViewById(R.id.person_tel);
+        mail = (TextView)findViewById(R.id.person_mail);
+        add = (TextView)findViewById(R.id.person_add);
+        name.setText(LoginActivity.person.getName());
+        
+    }
 
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_user, menu);
+        return true;
+    }*/
 }
+
